3a324f582018866c8bbbff256b66ae1e25ea5a18
pashdwmamena <pashdwamena@gmail.com>
2026-03-17 23:28:06 +0000

    Update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
+    base: '/berton-gear/',
     plugins: [
       vue({
         template: {
