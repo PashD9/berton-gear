26dd3add399f075283c370a0612cfdcca72117e1
pashdwmamena <pashdwamena@gmail.com>
2026-03-17 23:47:33 +0000

    Deploy: production build for gh-pages

---
+++ assets/CategoriesView-CDfJyG6-.js
@@ -0,0 +1,1 @@
+import{l as e,k as r,N as a,b as t}from"./index-DHXCq1Yb.js";const n={__name:"CategoriesView",setup(o){return(s,c)=>(t(),e(r(a)))}};export{n as default};
---
+++ assets/ProductsView-Xzumz9pZ.js
@@ -0,0 +1,1 @@
+import{l as e,k as r,N as t,b as a}from"./index-DHXCq1Yb.js";const n={__name:"ProductsView",setup(o){return(s,c)=>(a(),e(r(t)))}};export{n as default};
