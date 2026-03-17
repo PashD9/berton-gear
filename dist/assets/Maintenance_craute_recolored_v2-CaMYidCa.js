4a9bf9bde251b0b720dcb42c4f4cdcd3332b5b00
pashdwmamena <pashdwamena@gmail.com>
2026-03-17 18:45:30 +0000

    Stop tracking dist folder

--- dist/assets/Maintenance_craute_recolored_v2-CaMYidCa.js
+++
@@ -1,1 +0,0 @@
-const e="/berton-gear/assets/Maintenance_craute_recolored_v2-DywenHrF.svg";export{e as m};
--- dist/assets/ProductsView-Bvwusyg5.js
+++
@@ -1,1 +0,0 @@
-import{l as e,k as r,N as t,b as a}from"./index-DXBotdSo.js";const n={__name:"ProductsView",setup(o){return(s,c)=>(a(),e(r(t)))}};export{n as default};
